a81cd0b90bb76afde646c2b04f4067db47b77b43
yg-kim-korean <54833991+yg-kim-korean@users.noreply.github.com>
2022-02-05 00:44:03 +0900

    Initial commit

---
+++ src/main/java/com/server/wiiigglelunch/WiiiggleLunchApplication.java
@@ -0,0 +1,13 @@
+package com.server.wiiigglelunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiiiggleLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiiiggleLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/wiiigglelunch/WiiiggleLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.wiiigglelunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiiiggleLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
