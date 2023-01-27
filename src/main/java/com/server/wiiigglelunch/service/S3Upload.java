764a898569a403e35966ab39c398f5ddd869aaab
yg kim <kyg0752@gmail.com>
2023-01-27 16:47:57 +0900

    fin save AWS s3

--- src/main/java/com/server/wiiigglelunch/service/S3Upload.java
+++ src/main/java/com/server/wiiigglelunch/service/S3Upload.java
@@ -1,12 +1,14 @@
 package com.server.wiiigglelunch.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,13 @@ public class S3Upload {
     private final AmazonS3 amazonS3;
 
     public String upload(MultipartFile multipartFile) throws IOException{
-        St
+        String s3FileName = UUID.randomUUID()+"-"+ multipartFile.getOriginalFilename(); //랜덤 이름으로 생성
+
+        ObjectMetadata objectMeta = new ObjectMetadata(); //
+        objectMeta.setContentLength(multipartFile.getInputStream().available());
+
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objectMeta);
+
+        return amazonS3.getUrl(bucket,s3FileName).toString();
     }
 }
