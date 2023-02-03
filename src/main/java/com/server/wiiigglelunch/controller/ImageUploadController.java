15cec24de7a9c688ac69c7fea249fe6de7eb2e17
yg kim <kyg0752@gmail.com>
2023-02-03 16:11:18 +0900

    comment out about AWS s3

--- src/main/java/com/server/wiiigglelunch/controller/ImageUploadController.java
+++ src/main/java/com/server/wiiigglelunch/controller/ImageUploadController.java
@@ -1,24 +1,24 @@
-package com.server.wiiigglelunch.controller;
-
-import com.server.wiiigglelunch.service.S3Upload;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-@RequiredArgsConstructor
-@Controller
-public class ImageUploadController {
-    private  final S3Upload s3Upload;
-
-    @PostMapping("/photos")
-    public ResponseEntity<?> quploadImages(@RequestBody MultipartFile multipartFile) throws IOException {
-        s3Upload.upload(multipartFile);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-}
+//package com.server.wiiigglelunch.controller;
+//
+//import com.server.wiiigglelunch.service.S3Upload;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class ImageUploadController {
+//    private  final S3Upload s3Upload;
+//
+//    @PostMapping("/photos")
+//    public ResponseEntity<?> quploadImages(@RequestBody MultipartFile multipartFile) throws IOException {
+//        s3Upload.upload(multipartFile);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//}
--- src/main/java/com/server/wiiigglelunch/service/S3Upload.java
+++ src/main/java/com/server/wiiigglelunch/service/S3Upload.java
@@ -1,31 +1,31 @@
-package com.server.wiiigglelunch.service;
-
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.UUID;
-
-@Service
-@RequiredArgsConstructor
-public class S3Upload {
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    private final AmazonS3 amazonS3;
-
-    public String upload(MultipartFile multipartFile) throws IOException{
-        String s3FileName = UUID.randomUUID()+"-"+ multipartFile.getOriginalFilename(); //랜덤 이름으로 생성
-
-        ObjectMetadata objectMeta = new ObjectMetadata(); //
-        objectMeta.setContentLength(multipartFile.getInputStream().available());
-
-        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objectMeta);
-
-        return amazonS3.getUrl(bucket,s3FileName).toString();
-    }
-}
+//package com.server.wiiigglelunch.service;
+//
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.model.ObjectMetadata;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.util.UUID;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class S3Upload {
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
+//
+//    private final AmazonS3 amazonS3;
+//
+//    public String upload(MultipartFile multipartFile) throws IOException{
+//        String s3FileName = UUID.randomUUID()+"-"+ multipartFile.getOriginalFilename(); //랜덤 이름으로 생성
+//
+//        ObjectMetadata objectMeta = new ObjectMetadata(); //
+//        objectMeta.setContentLength(multipartFile.getInputStream().available());
+//
+//        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objectMeta);
+//
+//        return amazonS3.getUrl(bucket,s3FileName).toString();
+//    }
+//}
